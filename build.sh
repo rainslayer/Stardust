76ac52983759e0a0f60b0a95c1841aa2074e11f6
Don Rumata <r4inslayer@gmail.com>
2022-05-30 23:58:42 +0300

    Add submodules fetcing to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 BUILD_DIR="stardust_build"
+
+git submodule init
+git submodule update
+
 [[ -d "$BUILD_DIR" ]] || mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 cmake ../ && cmake --build . && make
